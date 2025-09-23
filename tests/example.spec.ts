6ecb97f4c588ba10e7f9fc8e16e49376732730dc
qaharvest <theqaharvest@gmail.com>
2025-09-23 17:48:51 -0400

    New file

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://www.andreandson.com/');
-  await expect(page.getByText('EXCELLENCE IN TURF PRODUCTS AND MANAGEMENT')).toContainText('EXCELLENCE IN TURF PRODUCTS AND MANAGEMENT');
+  
 });
 
