f923f2b2072eaa00845ff17c44f4cbe743c36331
qaharvest <theqaharvest@gmail.com>
2025-09-23 20:20:19 -0400

    Complete test

--- pages/example.ts
+++ pages/example.ts
@@ -1,1 +1,14 @@
-//Sample file 
+import { Locator, Page } from "@playwright/test";
+
+export class HomePage{
+
+    //variables 
+    readonly page: Page;
+    readonly textOnHeroSection: Locator;
+
+    constructor(page: Page){
+        this.page=page;
+        this.textOnHeroSection=page.getByText('EXCELLENCE IN TURF PRODUCTS AND MANAGEMENT');
+    }
+
+}
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,18 @@
 import { test, expect } from '@playwright/test';
+import { HomePage } from '../pages/example';
 
+/*
+Verify that the page title in the hero section "EXCELLENCE IN TURF PRODUCTS AND MANAGEMENT"
+*/
 test('has title', async ({ page }) => {
-  
+    const homePage = new HomePage(page);   
+    //Visit the web application 
+    await page.goto('/');
+    //Check that you are on the web page 
+    //await (page.url()).toHaveUrl();
+    //validate text matches with the hero section 
+    await expect(homePage.textOnHeroSection).toHaveText('Demo IN TURF PRODUCTS AND MANAGEMENT');
+   
+
 });
 
