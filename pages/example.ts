1220890cf2f8a2e96c53ad5fbd4a4c3ae6bf2a1e
tiffanyzqa <tzhang8472@gmail.com>
2025-09-26 13:22:06 -0400

    Verify Ice Melts Title

--- pages/example.ts
+++ pages/example.ts
@@ -1,1 +1,13 @@
-//Sample file 
+import { Locator, Page} from "@playwright/test";
+
+export class IceMeltsPage{
+
+    //variables
+    readonly page: Page;
+    readonly textOnHeroSection: Locator;
+
+    constructor(page:Page){
+        this.page=page;
+        this.textOnHeroSection=page.getByText('Ice Melts');
+    }
+}
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,17 @@
 import { test, expect } from '@playwright/test';
+import { IceMeltsPage } from '../pages/example';
+
+/*
+Verify that the page title is "ICE MELTS"
+*/
 
 test('has title', async ({ page }) => {
-  
+    const iceMeltsPage = new IceMeltsPage(page);
+　//Visit the web application
+  await page.goto('https://www.andreandson.com/ice-melts');
+  //Check that you are on the web page
+  await expect(page).toHaveURL('https://www.andreandson.com/ice-melts')
+  //Validate text matches with hero title
+  await expect(iceMeltsPage.textOnHeroSection).toHaveText('Ice Melts');
 });
 
