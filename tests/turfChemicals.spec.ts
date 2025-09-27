089fb6c63aa92d7af286f2bf625ad04f19904e44
AyeshaHabib032 <ayeshahabib023@gmail.com>
2025-09-27 08:43:51 -0500

    Added test cases for multiple Learn More buttons on Turf Chemicals page

--- tests/turfChemicals.spec.ts
+++ tests/turfChemicals.spec.ts
@@ -1,9 +1,9 @@
 import { test, expect } from '@playwright/test';
 
-/* Test case 132:Verify that clicking on "Learn More" button under "Albaugh" takes the user to "Albaugh" page
+/* Verify that clicking on "Learn More" button takes the user to their respective page
 */
 
-test('Test case 132:Verify that clicking on "Learn More" button under "Albaugh" takes the user to "Albaugh" page', async ({ page }) => {
+test('Verify that clicking on "Learn More" button takes the user to their respective page', async ({ page }) => {
   // Visit the web application
     await page.goto('https://www.andreandson.com/control-agents-2');
     
@@ -11,5 +11,61 @@ test('Test case 132:Verify that clicking on "Learn More" button under "Albaugh" 
   // click first learn more under Albaugh title
     const firstLearnMore = page.locator('text=Learn More').first();
     await firstLearnMore.click();
+
+      // Verify Albaugh page opened
+    await page.waitForLoadState('domcontentloaded');
+    await expect(page).toHaveURL('https://www.andreandson.com/prime-source');
+
+  // Go back to Turf Chemicals page
+    await page.goBack();
+    await page.waitForLoadState('domcontentloaded');
+    page.pause();
+    await expect(page).toHaveURL(/control-agents-2/);
+
+  // Click the second Learn More button
+    page.pause();
+    const secondLearnMore = page.locator('text=Learn More').nth(1);
+    await secondLearnMore.click();
+
+  // Verify user is on AMVAC page
+    await page.waitForLoadState('domcontentloaded');
+    await expect(page).toHaveURL('https://www.andreandson.com/amvac');
+
+      // Go back to Turf Chemicals page
+    await page.goBack();
+    await page.waitForLoadState('domcontentloaded');
+    page.pause();
+    await expect(page).toHaveURL(/control-agents-2/);
+
+      // Click the third Learn More button
+    page.pause();
+    const thirdLearnMore = page.locator('text=Learn More').nth(2);
+    await thirdLearnMore.click();
+
+      // Verify user is on BASF page
+    await page.waitForLoadState('domcontentloaded');
+    await expect(page).toHaveURL('https://www.andreandson.com/basf');
+
+          // Go back to Turf Chemicals page
+    await page.goBack();
+    await page.waitForLoadState('domcontentloaded');
+    page.pause();
+    await expect(page).toHaveURL(/control-agents-2/);
+
+       // Click the fourth Learn More button
+    page.pause();
+    const FourthLearnMore = page.locator('text=Learn More').nth(3);
+    await FourthLearnMore.click();
+
+     // Go back to Turf Chemicals page
+    await page.goBack();
+    await page.waitForLoadState('domcontentloaded');
+    page.pause();
+    await expect(page).toHaveURL(/control-agents-2/);
+
+      // Click the fifth Learn More button
+    page.pause();
+    const FifthLearnMore = page.locator('text=Learn More').nth(4);
+    await FifthLearnMore.click();
 });
 
