3341eab83d3fd64a36e5d5a22985d148a23bb384
jmentzer1 <jmfilmstudios@gmail.com>
2025-09-30 18:59:44 -0400

    chore(demo-baseurl-fix): commit local changes after successful tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -26,7 +26,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    // Read from env BASE_URL if provided, otherwise default to localhost:3000
+  baseURL: process.env.BASE_URL || 'https://www.andreandson.com',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
