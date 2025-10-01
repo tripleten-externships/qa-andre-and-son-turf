42b050537c5e831e99390c506a0ac605bc16fe91
95nacho <95nacho@gmail.com>
2025-09-30 17:51:57 -0700

    added message

--- tests/convAeroPage.spec.ts
+++ tests/convAeroPage.spec.ts
@@ -1,16 +1,24 @@
 import { test, expect } from '@playwright/test';
 
-/*
-    Test to verify that the "Conventional Aerification" page loads successfully.
+
+
+/*Verify page loads successfully
 */
-test('Page loads successfully', async ({ page }) => {
-    // Visit the web application
-    await page.goto('/');
-     // Drag mouse to CONTRACT SERVICES
-    await page.getByText('CONTRACT SERVICES').first().hover();
-    //Click on Conventional Aerification 
-    await page.getByRole('button', { name: 'CONVENTIONAL AERIFICIATION' }).click();
-    //Verify that page loads successfully 
-    expect(await page.url()).toEqual('https://www.andreandson.com/conventional-aerification');
-    
+test('page loads', async ({ page }) => {
+  //visit the home page
+  await page.goto('/');
+  //hover over to CONTRACT SERVICES
+  await page.getByRole('link', { name: 'CONTRACT SERVICES' }).nth(0).hover();
+  //Click on CONVENTIONAL AERIFICATION
+  if (await page.getByRole('link', { name: 'CONVENTIONAL AERIFICIATION' }).isVisible()) {
+  await page.getByRole('link', { name: 'CONVENTIONAL AERIFICIATION' }).click();
+    //Verify page loaded successfully
+  await expect(page).toHaveURL(/.*conventional-aerification/);
+  }
 });
+
+/* Verify images are displayed correctly
+*/
+//test('images display correctly', async ({ page }) => {
+
+//});
