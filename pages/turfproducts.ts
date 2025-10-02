18fe7424e87750ff4d7b221b229063a10e3ac05f
YuliyaF88 <yuliyafarber@gmail.com>
2025-10-02 14:12:23 -0400

    10/2 coommit

--- pages/turfproducts.ts
+++ pages/turfproducts.ts
@@ -2,55 +2,132 @@ import { Locator, Page } from "@playwright/test";
 
 export class TuftProductsPage{
     readonly page: Page;
+    readonly logoAndreSon: Locator;
+    readonly logoAndreSonlink: Locator;
     readonly turfProductsTitle: Locator;
     readonly turfProductsHeroTitle: Locator;
-    readonly turfProductsHeroText: Locator;
+    readonly turfProductsHeroText1: Locator;
+    readonly turfProductsHeroText2: Locator;
+    readonly turfProductsHeroText3: Locator;
     readonly turfProductsCatalogTitle: Locator;
-    readonly turfProductsCatalogText: Locator;
+    readonly turfProductsCatalogText1: Locator;
+    readonly turfProductsCatalogText2: Locator;
+    readonly turfProductsCatalogLink: Locator;
+    readonly turfProductsCatalogAltText: Locator;
     readonly combinationProductsTitle: Locator;
     readonly combinationProductsText: Locator;
+    readonly combinationProductsImageAltText: Locator;
+    readonly combinationProductsLink: Locator;
+    readonly cplearnMoreButton: Locator;
     readonly turfKingFertilizerTitle: Locator;
     readonly turfKingFetilizerText: Locator;
+    readonly turfKingFetilizerAltText: Locator;
+    readonly kfLearnMoreButton: Locator;
     readonly controlAgentsTitle: Locator;
     readonly controlAgentsText: Locator;
+    readonly controlAgentsImageAltText: Locator;
+    readonly caLearnMoreButton: Locator;
     readonly liquidFertilizerTitle: Locator;
     readonly liquidFertilizerText: Locator;
+    readonly liquidFertilizerAltText: Locator;
+    readonly lfLearnMoreButton: Locator;
     readonly seedTitle: Locator;
     readonly seedText: Locator;
+    readonly seedAltText: Locator;
+    readonly seedLearnMoreButton: Locator;
     readonly amendmentsConditionersTitle: Locator;
     readonly amendmentsConditionersText: Locator;
+    readonly amendmentsConditionersImageAltText: Locator;
+    readonly acLearnMoreButton: Locator;
     readonly topDressingBunkerSandTitle: Locator;
     readonly topDressingBunkerSandText: Locator;
+    readonly topDressingBunkerSandImageAltText: Locator;
+    readonly tdbsLearnMoreButton: Locator;
     readonly iceMeltTitle: Locator;
     readonly iceMeltText: Locator;
+    readonly iceMeltImageAltText: Locator;
+    readonly imLearnMoreButton: Locator;
 
     constructor(page: Page){
         this.page=page;
+        this.logoAndreSon=page.getByText('ANDRE & SON ');
+        this.logoAndreSonlink=page.getByRole('link', {name: 'Andre & Son'});
         this.turfProductsTitle=page.getByRole('link', {name: 'Turf Products'}).nth(0);
         this.turfProductsHeroTitle=page.getByText("There's a Reason We're the Turf King");
-        this.turfProductsHeroText=page.getByText('')
+        this.turfProductsHeroText1=page.getByText('Our Turf King line of fertilizers is a top of the line, proven winner.')
+        this.turfProductsHeroText2=page.getByText("We're the Turf King because we're the experts.");
+        this.turfProductsHeroText3=page.getByText("You've got a turf problem? We'll tell you the product to solve it. Between our knowledgeable sales team, and our position as an authorized dealer of hundreds of turf products, our team will work hand-in -hand with you to get you optimal results while sharing knowledge for you to use in the future!");
         this.turfProductsCatalogTitle=page.getByText('TURF PRODUCT CATALOG');
-        this.turfProductsCatalogText=page.getByText('Browse our product catalog of');
+        this.turfProductsCatalogText1=page.getByText('Browse our product catalog of');
+        this.turfProductsCatalogText2=page.getByText('. Have a question?');
+        this.turfProductsCatalogLink=page.getByRole('link', { name: 'urf products', exact: true });
+        this.turfProductsCatalogAltText=page.getByAltText('turf_king_fert.png');
         this.combinationProductsTitle=page.getByText('Combination Products', { exact: true });
         this.combinationProductsText=page.getByRole('paragraph').filter({ hasText: 'All of our combination' });
+        this.combinationProductsImageAltText=page.getByAltText('combination products.jpg');
+        this.combinationProductsLink=page.getByRole('link', { name: 'combination products' });
+        this.cplearnMoreButton=page.getByRole('link', { name: 'Learn More', exact: true }).nth(0);
         this.turfKingFertilizerTitle=page.getByText('Turf King Fertilizer', { exact: true });
         this.turfKingFetilizerText=page.getByText('Our fertilizers are premium');
+        this.turfKingFetilizerAltText=page.getByAltText('dry fert.jpg');
+        this.kfLearnMoreButton=page.getByRole('link', { name: 'Learn More', exact: true }).nth(1);
         this.controlAgentsTitle=page.getByText('Control Agents');
         this.controlAgentsText=page.getByText('Taking pride in teaming up');
+        this.controlAgentsImageAltText=page.getByAltText('control agents.jpg');
+        this.caLearnMoreButton=page.getByRole('link', { name: 'Learn More', exact: true }).nth(2);
         this.liquidFertilizerTitle=page.getByText('Liquid Fertilizer', { exact: true });
         this.liquidFertilizerText=page.getByText('We\'ve got what you need to');
+        this.liquidFertilizerAltText=page.getByAltText('liquid fert.jpg');
+        this.lfLearnMoreButton=page.getByRole('link', { name: 'Learn More', exact: true }).nth(3);
         this.seedTitle=page.getByText('Seed', { exact: true });
         this.seedText=page.getByText('Turf King seed is blended');
+        this.seedAltText=page.getByAltText('seed.jpg');
+        this.seedLearnMoreButton=page.getByRole('link', { name: 'Learn More', exact: true }).nth(4);
         this.amendmentsConditionersTitle=page.getByText('Amendments & Conditioners');
         this.amendmentsConditionersText=page.getByRole('paragraph').filter({ hasText: 'Golf, athletic sports,' });
+        this.amendmentsConditionersImageAltText=page.getByAltText('soil amendments.jpg');
+        this.acLearnMoreButton=page.getByRole('link', { name: 'Learn More', exact: true }).nth(5);
         this.topDressingBunkerSandTitle=page.getByText('Top Dressing & Bunker Sand', { exact: true });
         this.topDressingBunkerSandText=page.getByText('Enhance your turf\'s health');
+        this.topDressingBunkerSandImageAltText=page.getByAltText('topdressing.jpg');
+        this.tdbsLearnMoreButton=page.getByRole('link', { name: 'Learn More', exact: true }).nth(6);
         this.iceMeltTitle=page.getByText('Ice Melt', { exact: true });
         this.iceMeltText=page.getByRole('paragraph').filter({ hasText: 'Ice melt is designed to make' });
+        this.iceMeltImageAltText=page.getByAltText('ice melt.jpg');
+        this.imLearnMoreButton=page.getByRole('link', { name: 'Learn More', exact: true }).nth(7);
         }
     async clickTurfProductsTitle(){
-        await this.turfProductsHeroTitle.click();
+        await this.turfProductsTitle.click();
+    } 
+    async clickTurfProductsCatalogLink(){
+        await this.turfProductsCatalogLink.click();
     }
-
+    async clickLogoAndreSon(){
+        await this.logoAndreSonlink.click();
+    }
+    async clickCPLearnMoreButton(){
+        await this.cplearnMoreButton.click();
+    }
+    async clickKFLearnMoreButton(){
+        await this.kfLearnMoreButton.click();
+    }
+    async clickCALearnMoreButton(){
+        await this.caLearnMoreButton.click();
+    }
+    async clickLFLearnMoreButton(){
+        await this.lfLearnMoreButton.click();
+    }
+    async clickSeedLearnMoreButton(){
+        await this.seedLearnMoreButton.click();
+    }
+    async clickACLearnMoreButton(){
+        await this.acLearnMoreButton.click();
+    }   
+    async clickTDBSLearnMoreButton(){
+        await this.tdbsLearnMoreButton.click();
+    }
+    async clickIMLearnMoreButton(){
+        await this.imLearnMoreButton.click();
+    }
 
 }
---
+++ tests/turfproducts.spec.ts
@@ -0,0 +1,213 @@
+import { test, expect } from '@playwright/test';
+import { TuftProductsPage } from '../pages/turfproducts';
+//import { beforeEach } from 'node:test';
+
+//beforeEach(async ({ page }) => {
+    //Go to home page
+    //await page.goto('/');
+    //click on "Turf Products" title
+    //const turfProductsPage = new TuftProductsPage(page);
+    //await turfProductsPage.clickTurfProductsTitle();
+//}
+
+//* Verify that the description under title "There's a Reason We're the Turf King" is visible and readable by user
+//* Verify that clicking the logo "Andre & Son" would take the user back to the homepage
+test('verify TuftProducts Hero text and logo', async ({ page }) => {
+    const turfProductsPage = new TuftProductsPage(page);
+    //go to home page
+    await page.goto('/');
+    //click on "Turf Products" title
+    await turfProductsPage.clickTurfProductsTitle();
+    //assert that the hero title and description is visible
+    await expect(turfProductsPage.turfProductsHeroTitle).toBeVisible();
+    await expect(turfProductsPage.turfProductsHeroText1).toBeVisible();
+    await expect(turfProductsPage.turfProductsHeroText2).toBeVisible();
+    await expect(turfProductsPage.turfProductsHeroText3).toBeVisible();
+    //click on "Andre & Son" logo
+    await turfProductsPage.clickLogoAndreSon();
+    //assert that the url is correct
+    await expect(page).toHaveURL('https://www.andreandson.com/');
+});
+
+
+//* Verify that the title "TURF PRODUCT CATALOG" and description under is visible and readable by user
+//* Verify that clicking on "tuft products" hyperlink in the "TURF PRODUCT CATALOG" description opens the company's website "Turf Products" in another tab.
+//* Verify that "Turf King Fertilizer" logo has Alt text in Devtools
+test('verify "TURF PRODUCT CATALOG"', async ({ page }) => {
+    const turfProductsPage = new TuftProductsPage(page);
+    //go to home page
+    await page.goto('/');
+    //click on "Turf Products" title
+    await turfProductsPage.clickTurfProductsTitle();
+    //assert that "TURF PRODUCT CATALOG" title and description is visible
+    await expect(turfProductsPage.turfProductsCatalogTitle).toBeVisible();
+    await expect(turfProductsPage.turfProductsCatalogText1).toBeVisible();
+    await expect(turfProductsPage.turfProductsCatalogText2).toBeVisible();
+     //assert that the "turf king" logo has alt text
+    await expect(turfProductsPage.turfProductsCatalogAltText).toBeVisible();
+    //click on "tuft products" link in the text
+    await turfProductsPage.clickTurfProductsCatalogLink();
+    //assert that the url is correct
+    await expect(page).toHaveURL("https://www.andreandson.com/turf-products");
+});
+
+//* Verify that the title "Combination Products" and description under is visible and readable by user
+//* Verify the image under "Combination Products" has Alt text in Devtools. 
+//* Verify that clicking on "combination products" hyperlink in the "Combination Products" description takes the user to "Turf King Combination Products" page
+test('verify "Combination Products"', async ({ page }) => {
+    const turfProductsPage = new TuftProductsPage(page);
+    //go to home page
+    await page.goto('/');
+    //click on "Turf Products" title
+    await turfProductsPage.clickTurfProductsTitle();
+    //assert that "Combination Products" title and description is visible
+    await expect(turfProductsPage.combinationProductsTitle).toBeVisible();
+    await expect(turfProductsPage.combinationProductsText).toBeVisible();
+    //assert that the "Combination Products" image has alt text
+    await expect(turfProductsPage.combinationProductsImageAltText).toBeVisible();
+    //click on "combination products" link in the text
+    await turfProductsPage.combinationProductsLink.click();
+    //assert that the url is correct
+    await expect(page).toHaveURL("https://www.andreandson.com/turf-king-combo-products");
+});
+
+//* Verify that clicking on "Learn More" button under "Combination Products" takes the user to "Turf King Combination Products" page
+test('verify Learn more button in combination products', async ({ page }) => {
+    const turfProductsPage = new TuftProductsPage(page);
+    //go to home page
+    await page.goto('/');
+    //click on "Turf Products" title
+    await turfProductsPage.clickTurfProductsTitle();
+    //click on "Learn More" button under "Combination Products"
+    await turfProductsPage.clickCPLearnMoreButton();
+    //assert that the url is correct
+    await expect(page).toHaveURL("https://www.andreandson.com/turf-king-combo-products");
+});
+
+//* Verify that the description under title "Turf King Fertilizer" is visible and readable by user
+//* Verify the image under "Turf King Fertilizer" has Alt text in Devtools. 
+//* Verify that clicking on "Learn More" button under "Turf King Fertilizer" takes the user to "Turf King Fertilizer Products" page
+test('verify "Turf King Fertilizer" section', async ({ page }) => {
+    const turfProductsPage = new TuftProductsPage(page);
+    //go to home page
+    await page.goto('/');
+    //click on "Turf Products" title
+    await turfProductsPage.clickTurfProductsTitle();
+    //assert that "Turf King Fertilizer" title and description is visible
+    await expect(turfProductsPage.turfKingFertilizerTitle).toBeVisible();
+    await expect(turfProductsPage.turfKingFetilizerText).toBeVisible();
+    //assert that the "Turf King Fertilizer" image has alt text
+    await expect(turfProductsPage.turfKingFetilizerAltText).toBeVisible();
+    //click on "Learn More" button under "Turf King Fertilizer"
+    await turfProductsPage.clickKFLearnMoreButton();
+    //assert that the url is correct
+    await expect(page).toHaveURL("https://www.andreandson.com/turf-king-dry-fertilizer");
+});
+
+//* Verify that the description under title "Control Agents" is visible and readable by user
+//* Verify the image under "Control Agents" has Alt text in Devtools. 
+//* Verify that clicking on "Learn More" button under "Control Agents" takes the user to "Turf King Control Agents" page
+test('verify "Control Agents" section', async ({ page }) => {
+    const turfProductsPage = new TuftProductsPage(page);
+    //go to home page
+    await page.goto('/');
+    //click on "Turf Products" title
+    await turfProductsPage.clickTurfProductsTitle();
+    //assert that "Control Agents" title and description is visible
+    await expect(turfProductsPage.controlAgentsTitle).toBeVisible();
+    await expect(turfProductsPage.controlAgentsText).toBeVisible();
+    //assert that the "Control Agents" image has alt text
+    await expect(turfProductsPage.controlAgentsImageAltText).toBeVisible();
+    //click on "Learn More" button under "Control Agents"
+    await turfProductsPage.clickCALearnMoreButton();
+    //assert that the url is correct
+    await expect(page).toHaveURL("https://www.andreandson.com/control-agents-2");
+});
+
+//* Verify that the description under title "Liquid Fertilizer" is visible and readable by user
+//* Verify the image under "Liquid Fertilizer" has Alt text in Devtools. 
+//* Verify that clicking on "Learn More" button under "Liquid Fertilizer" takes the user to "Turf King Liquid Fertilizer" page
+test('verify "Liquid Fertilizer" section', async ({ page }) => {
+    const turfProductsPage = new TuftProductsPage(page);
+    //go to home page
+    await page.goto('/');
+    //click on "Turf Products" title
+    await turfProductsPage.clickTurfProductsTitle();
+    //assert that "Liquid Fertilizer" title and description is visible
+    await expect(turfProductsPage.liquidFertilizerTitle).toBeVisible();
+    await expect(turfProductsPage.liquidFertilizerText).toBeVisible();
+    //assert that the "Liquid Fertilizer" image has alt text
+    await expect(turfProductsPage.liquidFertilizerAltText).toBeVisible();
+    //click on "Learn More" button under "Liquid Fertilizer"
+    await turfProductsPage.clickLFLearnMoreButton();
+    //assert that the url is correct
+    await expect(page).toHaveURL("https://www.andreandson.com/liquid-fertilizer");
+});
+
+test('verify "Seed" section', async ({ page }) => {
+    const turfProductsPage = new TuftProductsPage(page);
+    //go to home page
+    await page.goto('/');
+    //click on "Turf Products" title
+    await turfProductsPage.clickTurfProductsTitle();
+    //assert that "Seed" title and description is visible
+    await expect(turfProductsPage.seedTitle).toBeVisible();
+    await expect(turfProductsPage.seedText).toBeVisible();
+    //assert that the "Seed" image has alt text
+    await expect(turfProductsPage.seedAltText).toBeVisible();
+    //click on "Learn More" button under "Seed"
+    await turfProductsPage.clickSeedLearnMoreButton();
+    //assert that the url is correct
+    await expect(page).toHaveURL("https://www.andreandson.com/seed");
+});
+
+test('verify "Amendments & Conditioners" section', async ({ page }) => {
+    const turfProductsPage = new TuftProductsPage(page);
+    //go to home page
+    await page.goto('/');
+    //click on "Turf Products" title
+    await turfProductsPage.clickTurfProductsTitle();
+    //assert that "Amendments & Conditioners" title and description is visible
+    await expect(turfProductsPage.amendmentsConditionersTitle).toBeVisible();
+    await expect(turfProductsPage.amendmentsConditionersText).toBeVisible();
+    //assert that the "Amendments & Conditioners" image has alt text
+    await expect(turfProductsPage.amendmentsConditionersImageAltText).toBeVisible();
+    //click on "Learn More" button under "Amendments & Conditioners"
+    await turfProductsPage.clickACLearnMoreButton();
+    //assert that the url is correct
+    await expect(page).toHaveURL("https://www.andreandson.com/soil-admendments");
+});
+
+test('verify "Top Dressing & Bunker Sand" section', async ({ page }) => {
+    const turfProductsPage = new TuftProductsPage(page);
+    //go to home page
+    await page.goto('/');
+    //click on "Turf Products" title
+    await turfProductsPage.clickTurfProductsTitle();
+    //assert that "Top Dressing & Bunker Sand" title and description is visible
+    await expect(turfProductsPage.topDressingBunkerSandTitle).toBeVisible();
+    await expect(turfProductsPage.topDressingBunkerSandText).toBeVisible();
+    //assert that the "Top Dressing & Bunker Sand" image has alt text
+    await expect(turfProductsPage.topDressingBunkerSandImageAltText).toBeVisible();
+    //click on "Learn More" button under "Top Dressing & Bunker Sand"
+    await turfProductsPage.clickTDBSLearnMoreButton();
+    //assert that the url is correct
+    await expect(page).toHaveURL("https://www.andreandson.com/top-dressing-bunker-sand");
+});
+
+test('verify "Ice Melt" section', async ({ page }) => {
+    const turfProductsPage = new TuftProductsPage(page);
+    //go to home page
+    await page.goto('/');
+    //click on "Turf Products" title
+    await turfProductsPage.clickTurfProductsTitle();
+    //assert that "Ice Melt" title and description is visible
+    await expect(turfProductsPage.iceMeltTitle).toBeVisible();
+    await expect(turfProductsPage.iceMeltText).toBeVisible();
+    //assert that the "Ice Melt" image has alt text
+    await expect(turfProductsPage.iceMeltImageAltText).toBeVisible();
+    //click on "Learn More" button under "Ice Melt"
+    await turfProductsPage.clickIMLearnMoreButton();
+    //assert that the url is correct
+    await expect(page).toHaveURL("https://www.andreandson.com/ice-melts");
+});
