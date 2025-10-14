dfd2a569ce375f34bb7d956139f2d9dc3346fbc9
tiffanyzqa <tzhang8472@gmail.com>
2025-10-14 13:13:37 -0400

    Updated Contract Services Scripts

--- pages/contractservices.ts
+++ pages/contractservices.ts
@@ -6,13 +6,26 @@ export class ContractServicesPage{
    readonly page: Page;
    readonly contractServicesTitle: Locator;
    readonly contractServicesVideo: Locator;
-   readonly conventionalAerificationelement: Locator;
+   readonly conventionalAerificationElement: Locator;
+   readonly deepTineAerificationElement: Locator;
+   readonly rotaryDecompactionElement: Locator;
+   readonly coreRemovalElement: Locator;
+   readonly applicationServicesElement: Locator;
+   readonly topdressingElement: Locator;
+   readonly seedingElement: Locator;
+   readonly constructionAndRenovationElement: Locator;
 
    constructor(page:Page) {
         this.page=page;
         this.contractServicesTitle = page.locator('h1, h2, [role="heading"]').first();
         this.contractServicesVideo = page.getByRole('region', { name: 'main content' }).locator('img');
-        this.conventionalAerificationelement = page.getByText('Conventional Aerification');
-
+        this.conventionalAerificationElement = page.getByText('Conventional Aerification');
+        this.deepTineAerificationElement = page.getByText('Deep Tine Aerification');
+        this.rotaryDecompactionElement = page.getByText('Rotary Decompaction', { exact: true });
+        this.coreRemovalElement = page.getByText('Core Removal', { exact: true });
+        this.applicationServicesElement = page.getByText('Application Services', { exact: true });
+        this.topdressingElement = page.getByText('Topdressing', { exact: true });
+        this.seedingElement = page.getByText('Seeding', { exact: true });
+        this.constructionAndRenovationElement = page.getByText('Construction & Renovation', { exact: true });
 }
 }
--- tests/contractservices.spec.ts
+++ tests/contractservices.spec.ts
@@ -28,15 +28,16 @@ test('Verify that the video plays in the hero when user land on Contract Service
     await expect(contractServicesPage.contractServicesVideo).toHaveJSProperty('ended', false);
 });
 */
+
 /*Verify that the "Conventional Aerification" link element is clickable and navigates to the correct page*/
-test.only('Verify that the "Conventional Aerification" link element is clickable and navigates to the correct page', async ({ page }) => {
+test('Verify that the "Conventional Aerification" link element is clickable and navigates to the correct page', async ({ page }) => {
     const contractServicesPage = new ContractServicesPage(page);
     //Go to Contract Services page
     await page.goto('/contract-services', { waitUntil: 'load' });
     //Click on the "Conventional Aerification" link element
-    await contractServicesPage.conventionalAerificationelement.scrollIntoViewIfNeeded();
-    await contractServicesPage.conventionalAerificationelement.isVisible();
-    await contractServicesPage.conventionalAerificationelement.click();
+    await contractServicesPage.conventionalAerificationElement.scrollIntoViewIfNeeded();
+    await contractServicesPage.conventionalAerificationElement.isVisible();
+    await contractServicesPage.conventionalAerificationElement.click();
     //Verify that the new page is opened with correct URL
     await expect(page.url()).toContain('/conventional-aerification');
     //Verify that the new page contains the expected content
@@ -44,10 +45,117 @@ test.only('Verify that the "Conventional Aerification" link element is clickable
     await expect(newPageTitle).toContainText('Conventional Aerification'); // Adjust the expected text as needed
 });
 
-/*Verify that the "Conventional Aerification" link element is clickable and navigates to the correct page*/
-
 /*Verify that the "Deep Tine Aerification" link element is clickable and navigates to the correct page*/
 test('Verify that the "Deep Tine Aerification" link element is clickable and navigates to the correct page', async ({ page }) => {
     const contractServicesPage = new ContractServicesPage(page);
+    //Go to Contract Services page
+    await page.goto('/contract-services', { waitUntil: 'load' });
+    //Click on the "Deep Tine Aerification" link element
+    await contractServicesPage.deepTineAerificationElement.scrollIntoViewIfNeeded();
+    await contractServicesPage.deepTineAerificationElement.isVisible();
+    await contractServicesPage.deepTineAerificationElement.click();
+    //Verify that the new page is opened with correct URL
+    await expect(page.url()).toContain('/deep-tine-aerificiation');
+    //Verify that the new page contains the expected content
+    const newPageTitle = page.locator('h1'); // Adjust the selector as needed
+    await expect(newPageTitle).toContainText('Deep Tine Aerification'); // Adjust the expected text as needed
     
+});
+
+/*Verify that the "Rotary Decompaction" link element is clickable and navigates to the correct page*/
+test('Verify that the "Rotary Decompaction" link element is clickable and navigates to the correct page', async ({ page }) => {
+    const contractServicesPage = new ContractServicesPage(page);
+    //Go to Contract Services page
+    await page.goto('/contract-services', { waitUntil: 'load' });
+    //Click on the "Rotary Decompaction" link element
+    await contractServicesPage.rotaryDecompactionElement.scrollIntoViewIfNeeded();
+    await contractServicesPage.rotaryDecompactionElement.isVisible();
+    await contractServicesPage.rotaryDecompactionElement.click();
+    //Verify that the new page is opened with correct URL
+    await expect(page.url()).toContain('/rotary-decompaction');
+    //Verify that the new page contains the expected content
+    const newPageTitle = page.locator('h1'); // Adjust the selector as needed
+    await expect(newPageTitle).toContainText('Rotary Decompaction'); // Adjust the expected text as needed
+
+});
+
+/*Verify that the "Core Removal" link element is clickable and navigates to the correct page*/
+test('Verify that the "Core Removal" link element is clickable and navigates to the correct page', async ({ page }) => {
+    const contractServicesPage = new ContractServicesPage(page);
+    //Go to Contract Services page
+    await page.goto('/contract-services', { waitUntil: 'load' });
+    //Click on the "Core Removal" link element
+    await contractServicesPage.coreRemovalElement.scrollIntoViewIfNeeded();
+    await contractServicesPage.coreRemovalElement.isVisible();
+    await contractServicesPage.coreRemovalElement.click();
+    //Verify that the new page is opened with correct URL
+    await expect(page.url()).toContain('/core-removal');
+    //Verify that the new page contains the expected content
+    const newPageTitle = page.locator('h1'); // Adjust the selector as needed
+    await expect(newPageTitle).toContainText('Core Removal'); // Adjust the expected text as needed
+
+});
+
+/*Verify that the "Application Services" link element in the footer is clickable and navigates to the correct page*/
+test('Verify that the "Application Services" link element in the footer is clickable and navigates to the correct page', async ({ page }) => {
+    const contractServicesPage = new ContractServicesPage(page);
+    //Go to Contract Services page
+    await page.goto('/contract-services', { waitUntil: 'load' });
+    //Click on the "Application Services" link element
+    await contractServicesPage.applicationServicesElement.scrollIntoViewIfNeeded();
+    await contractServicesPage.applicationServicesElement.isVisible();
+    await contractServicesPage.applicationServicesElement.click();
+    //Verify that the new page is opened with correct URL
+    await expect(page.url()).toContain('/application-services');
+    //Verify that the new page contains the expected content
+    const newPageTitle = page.locator('h1'); // Adjust the selector as needed
+    await expect(newPageTitle).toContainText('Application Services'); // Adjust the expected text as needed
+});
+
+/*Verify that the "Topdressing" link element in the footer is clickable and navigates to the correct page*/
+test('Verify that the "Topdressing" link element in the footer is clickable and navigates to the correct page', async ({ page }) => {
+    const contractServicesPage = new ContractServicesPage(page);
+    //Go to Contract Services page
+    await page.goto('/contract-services', { waitUntil: 'load' });
+    //Click on the "Topdressing" link element
+    await contractServicesPage.topdressingElement.scrollIntoViewIfNeeded();
+    await contractServicesPage.topdressingElement.isVisible();
+    await contractServicesPage.topdressingElement.click();
+    //Verify that the new page is opened with correct URL
+    await expect(page.url()).toContain('/topdressing');
+    //Verify that the new page contains the expected content
+    const newPageTitle = page.locator('h1'); // Adjust the selector as needed
+    await expect(newPageTitle).toContainText('Topdressing'); // Adjust the expected text as needed
+});
+
+/*Verify that the "Seeding" link element in the footer is clickable and navigates to the correct page*/
+test('Verify that the "Seeding" link element in the footer is clickable and navigates to the correct page', async ({ page }) => {
+    const contractServicesPage = new ContractServicesPage(page);
+    //Go to Contract Services page
+    await page.goto('/contract-services', { waitUntil: 'load' });
+    //Click on the "Seeding" link element
+    await contractServicesPage.seedingElement.scrollIntoViewIfNeeded();
+    await contractServicesPage.seedingElement.isVisible();
+    await contractServicesPage.seedingElement.click();
+    //Verify that the new page is opened with correct URL
+    await expect(page.url()).toContain('/seeding');
+    //Verify that the new page contains the expected content
+    const newPageTitle = page.locator('h1'); // Adjust the selector as needed
+    await expect(newPageTitle).toContainText('Seeding'); // Adjust the expected text as needed
+});
+
+/*Verify that the "Construction & Renovation" link element in the footer is clickable and navigates to the correct page*/
+test('Verify that the "Construction & Renovation" link element in the footer is clickable and navigates to the correct page', async ({ page }) => {
+    const contractServicesPage = new ContractServicesPage(page);
+    //Go to Contract Services page
+    await page.goto('/contract-services', { waitUntil: 'load' });
+     //Click on the "Construction & Renovation" link element
+    await contractServicesPage.constructionAndRenovationElement.scrollIntoViewIfNeeded();
+    await contractServicesPage.constructionAndRenovationElement.isVisible();
+    await contractServicesPage.constructionAndRenovationElement.click();
+    //Verify that the new page is opened with correct URL
+    await expect(page.url()).toContain('/construction-renovation');
+    //Verify that the new page contains the expected content
+    const newPageTitle = page.locator('h1'); // Adjust the selector as needed
+    await expect(newPageTitle).toContainText('Construction and Renovation'); // Adjust the expected text as needed
 });
