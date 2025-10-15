c659f3b4a4c0b523b1b52da63df0db5c18a6d53d
qaharvest <theqaharvest@gmail.com>
2025-10-15 18:53:08 -0400

    deleted files

--- tests/contacts.ts
+++
@@ -1,5 +0,0 @@
-import { test, expect } from '@playwright/test';
-import { homepage, } from '../ pages/homepage';
-import { contacts } from '../ pages/contacts';
-
-test.describe('Contacts Page Tests', () => {
